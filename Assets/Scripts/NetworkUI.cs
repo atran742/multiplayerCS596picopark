2dcc83f8253425b7af635a7edd3783d6e6bdbd51
atran742 <atran3420@sdsu.edu>
2026-03-07 14:01:08 -0800

    final changes

--- Assets/Scripts/NetworkUI.cs
+++ Assets/Scripts/NetworkUI.cs
@@ -3,20 +3,28 @@ using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using TMPro;
 
+/// <summary>
+///  uses ui buttons to start and connect client and host 
+/// </summary>
 public class NetworkUI : MonoBehaviour
 {
     public TMP_InputField ipInput;
 
     public void StartHost()
     {
+        //starts host when player clicks 
+        //allows for other clients to join the game 
         NetworkManager.Singleton.StartHost();
     }
 
     public void StartClient()
     {
-        var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
-        transport.ConnectionData.Address = ipInput.text;
+        //takes the entered ip address, and starts client 
+        //takes care of low-level network connection 
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = ipInput.text; //sets the address the client is trying to connect to 
 
+        //connects client to game 
         NetworkManager.Singleton.StartClient();
     }
 }
